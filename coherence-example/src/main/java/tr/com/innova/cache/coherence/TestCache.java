c94968e177e0e28b5209e5332b4334c68595232a
cihan.bulut.1979 <cihan.bulut.1979@gmail.com>
2019-02-07 10:13:06 +0300

    Coherence Cache

---
+++ coherence-example/src/main/java/tr/com/innova/cache/coherence/TestCache.java
@@ -0,0 +1,31 @@
+package tr.com.innova.cache.coherence;
+
+import com.tangosol.net.CacheFactory;
+import com.tangosol.net.NamedCache;
+
+public class TestCache {
+	public static void main(String[] args) {
+
+		CacheFactory.ensureCluster();
+		NamedCache cache = CacheFactory.getCache("hello-example");
+
+		for (int i = 0; i < 1000; i++) {
+			String key = "k" + i;
+			String value = "Hello World " + i;
+			cache.put(key, value);
+
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			System.out.println((String) cache.get(key) + " --> " + cache.getCacheService().getCluster().getLocalMember().getUid());
+			
+			
+
+		}
+
+		CacheFactory.shutdown();
+	}
+}
