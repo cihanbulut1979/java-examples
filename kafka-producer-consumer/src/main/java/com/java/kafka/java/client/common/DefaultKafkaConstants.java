63a05ccb1399baf6426959a0ae81efa0b7c962c8
cihan.bulut.1979 <cihan.bulut.1979@gmail.com>
2019-02-19 16:40:19 +0300

    Updated

--- kafka-producer-consumer/src/main/java/com/java/kafka/java/client/common/DefaultKafkaConstants.java
+++ kafka-producer-consumer/src/main/java/com/java/kafka/java/client/common/DefaultKafkaConstants.java
@@ -2,6 +2,6 @@ package com.java.kafka.java.client.common;
 
 public class DefaultKafkaConstants {
 	
-	public static String TOPIC_NAME ="topic-1";
-	public static String GROUP_ID_CONFIG = "consumer-1";
+	public static String TOPIC_NAME ="topic-2";
+	public static String GROUP_ID_CONFIG = "consumer-2";
 }
